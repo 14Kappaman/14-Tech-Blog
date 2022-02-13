6edd8d38bc2f672817def1111b43561bbd900c35
Aubrey Crook <aubreycrook@hotmail.com>
2022-02-13 10:54:50 -0500

    Created user, index and connection files to create user table in database for login form

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require("express")
+const session = require('express-session');
 require("dotenv").config()
-const {engine} = require("express-handlebars")
+const exphbs = require("express-handlebars")
 
 const port = process.env.PORT || 3000;
 const app = express();
+const sequelize = require ("./config/connection")
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-app.engine('handlebars', engine());
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+const hbs = exphbs.create({})
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set('views', './view');
+//app.set('views', './view');
+
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
@@ -19,5 +35,5 @@ app.get('/login', (req, res) => {
 app.listen(port, () => {
     console.log("Server is running. Please visit http://localhost:" + port + 
      " to view in development");
-
+     sequelize.sync({ force: false });
 })
